3665a1afab4d357b95b9ff3be13d43159ab3b024
Alen1ka <alena.karpukhina.99@mail.ru>
2022-09-29 12:00:10 +0700

    init commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-# from google_play_scraper import app, Sort, reviews
+from google_play_scraper import app, Sort, reviews
 from app_store_scraper import AppStore
 from pprint import pprint
 # from app_store_scraper import Podcast
@@ -15,7 +15,7 @@ def parser(user_request):
     df = pd.DataFrame(columns=['user_name', 'review', 'source', 'date', 'rating'])
     df.to_csv(f, mode='w', index=False)
 
-    # parser_google_play(user_request)
+    parser_google_play(user_request)
     parser_apple(user_request)
 
     # dict_reviews = ['Baked', 'Beans', 'shop', '23.03.1222', '2']
@@ -23,28 +23,12 @@ def parser(user_request):
 
 
 def parser_google_play(user_request):
-    '''result = app(
+    result = app(
         'com.nianticlabs.pokemongo',
-        lang='ru',  # defaults to 'en'
-        country='ru'  # defaults to 'us'
+        lang='en',  # defaults to 'en'
+        country='us'  # defaults to 'us'
     )
     print(result)
-    result, continuation_token = reviews(
-        'com.fantome.penguinisle',
-        # lang='en',  # defaults to 'en'
-        # country='us',  # defaults to 'us'
-        # sort=Sort.NEWEST,  # defaults to Sort.NEWEST
-        # count=3,  # defaults to 100
-        # filter_score_with=5  # defaults to None(means all score)
-    )
-
-    # If you pass `continuation_token` as an argument to the reviews function at this point,
-    # it will crawl the items after 3 review items.
-    result, _ = reviews(
-        'com.fantome.penguinisle',
-        continuation_token=continuation_token  # defaults to None(load from the beginning)
-    )
-    print(result)'''
     return 0
 
 
@@ -61,23 +45,6 @@ def parser_apple(user_request):
     df2 = df.join(pd.DataFrame(df.pop('review').tolist()))
     df2.head()
     df2.to_csv('/Users/Desktop/App Store Review tiktok.csv')
-    '''tiktok = AppStore(country="us", app_name="tiktok")
-    tiktok.review(how_many=15)
-    reviews = []
-    for review_data in tiktok.reviews:
-        # print(review_data)
-        user_name = review_data['userName']
-        review = review_data['review']
-        source = 'apple'
-        date = review_data['date']
-        rating = review_data['rating']
-        reviews.append({'user_name': user_name, 'review': review, 'source': source, 'date': date, 'rating': rating})
-    # pprint(reviews)
-    # print(np.array(tiktok.reviews))
-    df = pd.DataFrame(np.array(reviews), columns=['review'])
-    df2 = df.join(pd.DataFrame(df.pop('review').tolist()))
-    df2.head()
-    df2.to_csv(f"{user_request}.csv", mode='a', index=False, header=False)'''
     return 0
 
 
