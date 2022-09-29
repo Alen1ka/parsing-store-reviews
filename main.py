96abfe14f8d059a160708a815e25aa3f2b8098ac
Alen1ka <alena.karpukhina.99@mail.ru>
2022-09-29 12:07:19 +0700

    ошибки при работе с библиотеками google_play_scraper и app_store_scraper, начала сначала

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import pandas as pd
-from google_play_scraper import app, Sort, reviews
-from app_store_scraper import AppStore
 from pprint import pprint
-# from app_store_scraper import Podcast
-# from itunes_app_scraper.scraper import AppStoreScraper
 import numpy as np
 import json
 from bs4 import BeautifulSoup
@@ -23,28 +19,10 @@ def parser(user_request):
 
 
 def parser_google_play(user_request):
-    result = app(
-        'com.nianticlabs.pokemongo',
-        lang='en',  # defaults to 'en'
-        country='us'  # defaults to 'us'
-    )
-    print(result)
     return 0
 
 
 def parser_apple(user_request):
-    tiktok = AppStore(country="us", app_name="tiktok")
-    tiktok.review(how_many=15)
-    print(tiktok.reviews)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-    url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=iphone &_sacat=0&LH_TitleDesc=0&Model=Apple%20iPhone%208&_sop=12&LH_PrefLoc=0&rt=nc&Storage%20Capacity=64%20GB&_dcat=9355'
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'lxml')
-    df = pd.DataFrame(np.array(tiktok.reviews), columns=['review'])
-    df2 = df.join(pd.DataFrame(df.pop('review').tolist()))
-    df2.head()
-    df2.to_csv('/Users/Desktop/App Store Review tiktok.csv')
     return 0
 
 
